c7da36954a5f65b53a4b23d2479a3aad818e7769
Alexander Johnson <alexx.johnson@gmail.com>
2018-09-06 23:02:09 -0700

    Overhaul

---
+++ README.md
@@ -0,0 +1,6 @@
+# dotfiles
+
+* vim
+* zsh
+* terminal theme from https://github.com/sindresorhus/terminal-snazzy
+* requires antibody
--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
-cd
-curl -O https://raw.githubusercontent.com/rupa/z/master/z.sh
-mv z.sh .z.sh
-cat ". .z.sh" >> ~/.bash_profile
+#!/bin/bash
+
+
+delete_and_link() {
+    rm -r $HOME/$2
+    echo "Linking $PWD/$1 to $HOME/$2"
+    ln -s $PWD/$1 $HOME/$2
+  }
+
+# Vim setup
+curl -fLo ./vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+delete_and_link vim .vim
+delete_and_link vim/.vimrc .vimrc
+
+# zsh setup
+delete_and_link zsh/.zshrc .zshrc
+delete_and_link zsh/.zsh_plugins.txt .zsh_plugins.txt
+
+# z.sh
+delete_and_link z.sh .z.sh
