c5a04c407e1701465a4ec36a51ad381c07171fca
Xander Johnson <xander@metasyn.pw>
2019-12-07 12:32:01 -0800

    Add a nim & pyenv script.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
 
 delete_and_link() {
     rm -r $HOME/$2 2> /dev/null
@@ -6,37 +9,46 @@ delete_and_link() {
     ln -s $PWD/$1 $HOME/$2
   }
 
-# Vim setup
-echo
-echo "Getting vim plug..."
-curl -fLo ./vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-delete_and_link vim .vim
-delete_and_link vim/.vimrc .vimrc
+function setup_vim() {
+  echo
+  echo "Getting vim plug..."
+  curl -fLo ./vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  delete_and_link vim .vim
+  delete_and_link vim/.vimrc .vimrc
 
-mkdir -p ~/.config/nvim
-delete_and_link neovim/init.vim .config/nvim/init.vim
+  mkdir -p ~/.config/nvim
+  delete_and_link neovim/init.vim .config/nvim/init.vim
+}
 
-# zsh setup
-echo
-echo "Getting antibody & zsh stuff..."
-if ! [ -x "$(command -v antibody)" ]; then
-  curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
-fi
 
-delete_and_link zsh/.zshrc .zshrc
-delete_and_link zsh/.zsh_plugins.txt .zsh_plugins.txt
+function setup_zsh() {
+  echo
+  echo "Getting antibody & zsh stuff..."
+  [[ ! -x "$(command -v antibody)" ]] && curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
 
-# tmux
-echo
-echo "Getting tmux stuff..."
-delete_and_link tmux/.tmux.conf .tmux.conf
+  delete_and_link zsh/.zshrc .zshrc
+  delete_and_link zsh/.zsh_plugins.txt .zsh_plugins.txt
+}
+
+function setup_tmux() {
+  echo
+  echo "Getting tmux stuff..."
+  delete_and_link tmux/.tmux.conf .tmux.conf
+}
 
 # z.sh
-echo
-echo "Getting z.sh..."
-delete_and_link z.sh .z.sh
+function setup_misc() {
+  echo
+  echo "Getting z.sh..."
+  delete_and_link z.sh .z.sh
 
-# colors
-echo
-echo "Getting dircolors for nicer ls output..."
-delete_and_link terminal/dircolors.ansi-dark .dircolors.ansi-dark
+  echo
+  echo "Getting dircolors for nicer ls output..."
+  delete_and_link terminal/dircolors.ansi-dark .dircolors.ansi-dark
+}
+
+setup_zsh
+setup_vim
+setup_tmux
+setup_misc
+
