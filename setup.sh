bf6e23e1acc250a230b2515e44b71f1f6f485e30
Xander Johnson <xander@metasyn.pw>
2019-12-07 16:13:11 -0800

    Well, its sort of prettier output now.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DISTRO=
+
 set -o nounset
 
 function info() {
@@ -8,18 +10,91 @@ function info() {
 }
 
 function item() {
-  justified=$(printf "[%-15s%s] " $1)
-  $1 | awk -vT="$justified " '{print T  $0}'
+  justified=$(printf "\033[%sm[%-12s%s] \033[0m" $1 $2)
+  $2 | awk -vT="$justified " '{print T  $0}'
   echo
 }
 
-function check() {
+function random_color() {
+  RED='0;31'
+  GREEN='0;32'
+  ORANGE='0;33'
+  BLUE='0;34'
+  PURPLE='0;35'
+  CYAN='0;36'
+  LIGHTRED='1;31'
+  LIGHTGREEN='1;32'
+  YELLOW='1;33'
+  LIGHTBLUE='1;34'
+  LIGHTPURPLE='1;35'
+  LIGHTCYAN='1;36'
+  WHITE='1;37'
+
+  declare -a colors=(
+    $RED
+    $GREEN
+    $ORANGE
+    $BLUE
+    $PURPLE
+    $CYAN
+    $LIGHTRED
+    $LIGHTGREEN
+    $YELLOW
+    $LIGHTBLUE
+    $LIGHTPURPLE
+    $LIGHTCYAN
+    $WHITE
+  )
+  size=${#colors[@]}
+  idx=$(($RANDOM % $size))
+  echo ${colors[$idx]}
+}
+
+function missing() {
   if [[ ! -z "$(command -v $1)" ]]; then
     return 1
   else
     return 0
   fi
 }
+
+function setup_os() {
+  name=$(uname -a)
+  if [[ $name == *"Ubuntu"* ]] || [[ $name == *"Debian" ]]; then
+    DISTRO=debian
+    bash -c "sudo apt-get update"
+  fi
+  if [[ $name == *"Darwin"* ]]; then
+    DISTRO=macos
+
+    if $(missing "brew"); then
+      info "Getting homebrew..."
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+  fi
+}
+
+function pkgmgr() {
+  name=$(uname -a)
+  if [[ $DISTRO == "debian" ]]; then
+    bash -c "sudo apt-get $@"
+  fi
+
+  if [[ $DISTRO == "macos" ]]; then
+    bash -c "brew $@"
+  fi
+}
+
+function install() {
+  name=$(uname -a)
+  if [[ $name == *"Darwin"* ]]; then
+    pkgmgr "install $@"
+  else
+    pkgmgr "install -y $@"
+  fi
+}
+
 
 delete_and_link() {
     rm -r $HOME/$2 2> /dev/null
@@ -58,7 +133,7 @@ function setup_zsh() {
 
 
 function setup_python() {
-  if $(check "pyenv"); then
+  if $(missing "pyenv"); then
     info "Getting pyenv..." 
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
   else
@@ -67,11 +142,20 @@ function setup_python() {
 }
 
 function setup_nim() {
-  if $(check "nim") ; then
+  if $(missing "nim") ; then
     info "Getting nim..."
     curl https://nim-lang.org/choosenim/init.sh -sSf | sh
   else
     info "Nim is installed."
+  fi
+}
+
+function setup_node() {
+  if $(missing "node"); then
+    install nodejs
+  fi
+  if $(missing "npm"); then
+    install npm
   fi
 }
 
@@ -91,16 +175,18 @@ function setup_misc() {
 }
 
 declare -a setups=(
+  "setup_os"
   "setup_zsh"
   "setup_vim"
   "setup_python"
   "setup_nim"
+  "setup_node"
   "setup_tmux"
   "setup_misc"
 )
 
 for setup in ${setups[@]}; do
-  item $setup
+  item "$(random_color)" $setup
 done
 
 echo "🎉 All done!"
