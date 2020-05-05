edc972425e82d64120e8a0a8ac4d2c897ffcd7b5
Xander Johnson <xander@metasyn.pw>
2020-05-05 15:38:33 -0700

    Fuck you, Ruby.

--- setup_server.sh
+++ setup_server.sh
@@ -42,41 +42,6 @@ function setup_docker_compose {
 }
 
 
-function setup_standardnotes {
-  info "Setting up requirements for standardnotes..."
-
-  dpkg -l libmysqlclient-dev 2>&1 > /dev/null
-  if [[ $? -ne 0 ]]; then
-    info "Installing libsqlclient-dev..."
-    apt-get update && apt-get install libmysqlclient-dev -y
-  else
-    info "libmysqlclient-dev exists..."
-  fi
-
-  if $(missing "ruby"); then
-    info "Installing ruby..."
-    apt-get update && apt-get install ruby-full=1:2.5.1 -y
-    ln -s /usr/bin/ruby2.5 /usr/bin/ruby
-    ln -s /usr/bin/gem2.5 /usr/bin/gem
-  else
-    info "Ruby installed..."
-  fi
-
-  if [[ ! -d  /opt/syncing-server ]]; then
-    cd /opt
-    git clone https://github.com/standardnotes/syncing-server.git
-    cd syncing-server
-    gem install bundler
-    bundle install
-    info "Please copy .env.sample to .env and update it before launching."
-    info "Then run: docker-compose up -d"
-  else
-    info "Syncing server exists..."
-  fi
-
-}
-
-
 function setup_ufw {
   ufw default deny incoming
   ufw default allow outgoing
@@ -90,7 +55,6 @@ function setup_ufw {
 declare -a setups=(
   "setup_docker"
   "setup_docker_compose"
-  "setup_standardnotes"
   "setup_ufw"
 )
 
